7408896e3afbb0ec9b80d7418eddf9286919b7cd
bgevko <bgevko@gmail.com>
2026-06-23 17:43:54 -0500

    feat: type common xel wrapper props

--- src/types.ts
+++ src/types.ts
@@ -32,9 +32,21 @@ export type XelBooleanProps = {
   togglable?: Booleanish;
 };
 
+export type XelCommonProps = {
+  href?: string;
+  icon?: string;
+  id?: string;
+  max?: number | string;
+  min?: number | string;
+  skin?: string;
+  step?: number | string;
+  value?: string | number | null;
+};
+
 export type XelComponentProps<TElement extends HTMLElement = HTMLElement> =
-  Omit<JSX.HTMLAttributes<TElement>, keyof XelBooleanProps> &
+  Omit<JSX.HTMLAttributes<TElement>, keyof XelBooleanProps | keyof XelCommonProps> &
     XelBooleanProps &
+    XelCommonProps &
     XelEventProps<TElement> & {
       children?: JSX.Element;
       properties?: Partial<TElement> & Record<string, unknown>;
